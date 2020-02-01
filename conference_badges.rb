aa9dd07f2e630987879d5993d3023ad37f960fd2
TShipley <taneymail@gmail.com>
2020-02-01 19:32:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 
 def batch_badge_creator(attendees)
-   attendees do |attendee|
+   attendees.each do |attendee|
    puts "Hello, my name is #{attendee}."
   end
 end
