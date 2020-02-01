bb7d446937eb545458cf3d27ec205c5e01e43afe
TShipley <taneymail@gmail.com>
2020-02-01 19:06:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,21 @@
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end  
+
+
 attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 
-attendees.each do |name|
-  puts "Hello, my name is #{name}."
+def batch_badge_creator(attendees)
+   attendees.each do |attendee|
+   puts "Hello, my name is #{attendee}."
+  end
 end
 
 def assign_rooms(attendees)
+   attendees.each_with_index do |attendee, index|
+   puts "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+end
+  
+  
   
