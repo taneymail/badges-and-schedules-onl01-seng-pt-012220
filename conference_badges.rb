1af4d3b43d56ab8e009b3a23955649e53a7a137a
TShipley <taneymail@gmail.com>
2020-02-01 18:33:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,7 @@
 batch_badge_creator = [Edsger, Ada, Charles, Alan, Grace, Limus, Matz]
-batch_badge_creator.each do |name|
-  puts "Hello, my name is #{name}."
+
+count = 0 
+while count <= batch_badge_creator.length-1
+  puts "Hello, my name is #{batch_badge_creator[count]}."
+  count += 1
 end
