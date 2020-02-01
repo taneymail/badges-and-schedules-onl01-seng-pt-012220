27c951046c5812b1877e8d60ec26274b948387b7
TShipley <taneymail@gmail.com>
2020-02-01 18:40:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
-speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 
 count = 0 
-while count <= speakers.length -1
-  puts "Hello, my name is #{speakers[count]}."
+while count <= attendees.length -1
+  puts "Hello, my name is #{attendees[count]}."
   count += 1
 end
