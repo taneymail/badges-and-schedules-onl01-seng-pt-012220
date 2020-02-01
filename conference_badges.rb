b8e643f25fbb9030e49fab11b8fe8aab47e5be50
TShipley <taneymail@gmail.com>
2020-02-01 19:54:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,3 +16,12 @@ def assign_rooms(attendees)
   attendees.each_with_index do |name, index|
     new_attendees << "Hello, #{name}! You'll be assigned to room #{index+1}!"
   
+def printer(arr)
+  batch_badge_creator(arr).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(arr).each do |assignment|
+    puts assignment
+  end
+end 
